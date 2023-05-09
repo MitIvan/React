aea79fb89d2ce6dfe78b0beac0c66df7592db683
Ivan Mitev <ivan.mitev90@gmail.com>
2023-05-09 15:56:00 +0200

    finish

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Counter from './components/Counter';
+import {Fragment} from 'react';
+import Header from './components/Header';
+import Auth from './components/Auth';
+// import userProfile from './components/UserProfile';
+import {useSelector} from 'react-redux';
+import UserProfile from './components/UserProfile';
+
+
+
+function App() {
+    const isAuth = useSelector(state => state.auth.isAuth)
+  return (
+      <Fragment>
+        <Header />
+          {!isAuth && <Auth />}
+          {isAuth && <UserProfile/>}
+        <Counter />
+      </Fragment>
+
+  );
+}
+
+export default App;
